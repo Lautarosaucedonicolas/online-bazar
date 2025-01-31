9aba9e8db84bf881153788c1fe9ee42065d60019
lautarosaucedonicolas <lautarosaucedonicolas@gmail.com>
2025-01-31 01:57:58 -0300

    header,carrusel,wsp listo

---
+++ script.js
@@ -0,0 +1,7 @@
+const botonesAgregar = document.querySelectorAll('.producto button');
+
+botonesAgregar.forEach(boton => {
+    boton.addEventListener('click', () => {
+        alert('Producto agregado al carrito');
+    });
+});
