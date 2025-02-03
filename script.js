14bd7a59fa2f8e5bf10a3e4bba3b062ef3d2f88d
lautarosaucedonicolas <lautarosaucedonicolas@gmail.com>
2025-02-03 19:34:56 -0300

    modulo de express achivo script.js como archivo index.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+const express = require('express'); // Importa el módulo Express
+const app = express(); // Crea una instancia de la aplicación Express
+const port = 3000; // Define el puerto en el que escuchará el servidor
+
+// Define una ruta para la página principal ("/")
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Envía el archivo index.html al cliente
+});
+
+// Inicia el servidor y escucha en el puerto especificado
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`); // Muestra un mensaje en la consola cuando el servidor se inicia
+});
+
+
+
+
+
+
+
+
+
+
+
 const botonesAgregar = document.querySelectorAll('.producto button');
 
 botonesAgregar.forEach(boton => {
