cd2215748ebe478991aa01680bcf0535f7b7ca8b
lautarosaucedonicolas <lautarosaucedonicolas@gmail.com>
2025-02-03 20:57:14 -0300

    nueva organizacion de directorio

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require('express'); // Importa el módulo Express
 const app = express(); // Crea una instancia de la aplicación Express
 const port = 3000; // Define el puerto en el que escuchará el servidor
 
+
+
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
+
+
 // Define una ruta para la página principal ("/")
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Envía el archivo index.html al cliente
+  res.sendFile(__dirname + '/public/index.html'); // Envía el archivo index.html al cliente
 });
 
 // Inicia el servidor y escucha en el puerto especificado
